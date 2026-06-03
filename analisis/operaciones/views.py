6ab933bd5f8eb58094932f8d392a4640fda4cb5d
JuanHU996 <franciscohuacon05@gmail.com>
2026-06-02 22:09:48 -0500

    Add files via upload

---
+++ analisis/operaciones/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+
+# Create your views here.
+def inicio(request):
+    contexto = {}
+
+    if request.method == 'POST':
+        numeros_texto = request.POST.get('numeros', '')
+        numeros = [int(n) for n in numeros_texto.split() if n.isdigit()]
+
+        operaciones = request.POST.getlist('operaciones')
+
+        if numeros:
+            contexto['numeros'] = numeros
+
+            if 'suma' in operaciones:
+                contexto['suma'] = sum(numeros)
+            if 'promedio' in operaciones:
+                contexto['promedio'] = sum(numeros) / len(numeros)
+            if 'mayor' in operaciones:
+                contexto['mayor'] = max(numeros)
+            if 'menor' in operaciones:
+                contexto['menor'] = min(numeros)
+            if 'pares' in operaciones:
+                contexto['pares'] = list(filter(lambda x: x % 2 == 0, numeros))
+            if 'impares' in operaciones:
+                contexto['impares'] = list(filter(lambda x: x % 2 != 0, numeros))
+            if 'cuadrados' in operaciones:
+                contexto['cuadrados'] = list(map(lambda x: x ** 2, numeros))
+
+    return render(request, 'base.html', contexto)
